2c618a269ad6b95eeffc84341b082f92240ca30a
Matthias Van Woensel <matthias@insypro.com>
2018-06-27 14:34:54 +0200

    fixing layout

--- project/assets/templates/pages/tourist/edit.tpl.php
+++ project/assets/templates/pages/tourist/edit.tpl.php
@@ -1,6 +1,8 @@
 <div class="wrapper-content">
 	<div class="row">
 		<?php $_CONTROL->pnlTourist->Render();?>
+	</div>
+	<div class="row" style="text-align: right">
 		<?php $_CONTROL->btnSave->Render();?>
 	</div>
 	<div class="row">
